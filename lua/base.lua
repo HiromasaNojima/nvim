3deb2b2ec0f9d2bcb88c1d3d962aff31fdfe94e0
HiromasaNojima <67505578+HiromasaNojima@users.noreply.github.com>
2022-12-04 19:33:10 +0900

    init

---
+++ lua/base.lua
@@ -0,0 +1,15 @@
+-- ヤンクした時に、clipboard に コピーできるように
+vim.opt.clipboard = 'unnamedplus'
+-- 制御文字の設定
+vim.opt.list = true
+vim.opt.listchars = {
+	tab = '│·',
+	extends = '⟩',
+	precedes = '⟨',
+	trail = '·',
+	eol = '↴',
+	nbsp = '%'
+}
+-- インデント周りの設定
+vim.opt.ts = 4
+vim.opt.shiftwidth = 4
---
+++ lua/plugins.lua
@@ -0,0 +1,56 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+	use { "wbthomason/packer.nvim", opt = true }
+	-- nvim-tree
+	use {
+  		'nvim-tree/nvim-tree.lua',
+		wants = "nvim-web-devicons", 
+		tag = 'nightly',
+		config = function()
+			require("nvim-web-devicons").setup()
+			require("nvim-tree").setup({
+				open_on_setup = true,
+
+			})
+		end	
+	}
+	-- lsp
+	use "neovim/nvim-lspconfig"
+
+	-- cmp
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/nvim-cmp'
+	
+	use 'L3MON4D3/LuaSnip'
+	use 'saadparwaiz1/cmp_luasnip'
+	-- {} とか自動で閉じてくれるやつ
+	use {
+		"windwp/nvim-autopairs",
+		config = function() require("nvim-autopairs").setup {} end
+	}
+
+	use { "fatih/vim-go", opt = true, ft = { "go" } }
+
+	use "github/copilot.vim"
+
+	use "tpope/vim-commentary"
+
+	if packer_bootstrap then
+    		require('packer').sync()
+  	end
+end)

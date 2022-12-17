51ac4e44f0b34815b4f2b945159d5cbe9866075c
HiromasaNojima <67505578+HiromasaNojima@users.noreply.github.com>
2022-12-17 17:59:02 +0900

    add key map to nvimtreetoggle

--- lua/nvim-tree-setting.lua
+++ lua/nvim-tree-setting.lua
@@ -4,4 +4,5 @@ require("nvim-tree").setup({
 	open_on_tab = true,
 })
 
+vim.api.nvim_set_keymap("n", "<C-b>", ":NvimTreeToggle<CR>", {noremap = true, silent = true})
 -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close
